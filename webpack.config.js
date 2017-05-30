897256d4661721e8911f43e134732b04d94dde27
amir-halperin <amiram.halperin@gmail.com>
2017-05-30 18:00:35 +0300

    integrate webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 var weback = require('webpack');
+var path = require('path');
+
 module.exports = {
     entry: './app.ts',
     output: {
-        filename: './js/bundle.js'
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '',
+        filename: 'bundle.js'
     },
     devtool: 'source-map',
     resolve: {
-        extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+        extensions: ['.webpack.js', '.web.js', '.ts', '.js', '.html']
     },
     plugins: [
-        new weback.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
-        })
+        // new weback.optimize.UglifyJsPlugin({
+        //     compress: { warnings: false }
+        // })
     ],
     module: {
         loaders: [
-            { test: /\.ts$/, loader: 'ts-loader' }
+            { test: /\.(ts)$/, loader: 'ts-loader' }
         ]
     }
 }
